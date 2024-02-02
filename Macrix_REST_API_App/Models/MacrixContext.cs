648d5b8bb4cf76c74623c43b386eed06e6e79a0a
matrinkoszak <mateusz.ryfa97@protonmail.com>
2024-02-02 22:08:53 +0100

    db_model_creation

---
+++ Macrix_REST_API_App/Models/MacrixContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Macrix_REST_API_App.Models;
+
+public partial class MacrixContext : DbContext
+{
+    public MacrixContext()
+    {
+    }
+
+    public MacrixContext(DbContextOptions<MacrixContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Person> People { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlite("DataSource=.\\Database\\macrix.db");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Person>(entity =>
+        {
+            entity.ToTable("person");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.FirstName).HasColumnType("TEXT (32)");
+            entity.Property(e => e.LastName).HasColumnType("TEXT (32)");
+            entity.Property(e => e.PostalCode).HasColumnType("TEXT (6)");
+            entity.Property(e => e.StreetName).HasColumnType("TEXT (32)");
+            entity.Property(e => e.Town).HasColumnType("TEXT (32)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Macrix_REST_API_App/Models/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Macrix_REST_API_App.Models;
+
+public partial class Person
+{
+    public long Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string StreetName { get; set; } = null!;
+
+    public long HouseNumber { get; set; }
+
+    public long? ApartmentNumber { get; set; }
+
+    public string PostalCode { get; set; } = null!;
+
+    public string Town { get; set; } = null!;
+
+    public long PhoneNumber { get; set; }
+
+    public string DateOfBirth { get; set; } = null!;
+}
